e143cb441da243ca66b4e747ed523454509713b1
David Jankovic <davidjankovic0703@gmail.com>
2016-12-14 10:26:17 +0100

    step1

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,9 @@
 <?php
 	
 	$w_routes = array(
-		['GET', '/', 'Default#home', 'default_home'],
+		['GET', 		'/', 										'Default#home', 			'default_home'],
+		['GET|POST',	'/login',									'Default#login',			'default_login'],
+		['GET',			'/logout',									'Default#logout',			'default_logout'],
+		['GET|POST',	'/user/password-recovery',					'Default#passwordRecovery',	'default_password_recovery'],
+		['GET|POST',	'/user/reset-password/[:token]',			'Default#resetPassword',	'default_reset_password'],
 	);
