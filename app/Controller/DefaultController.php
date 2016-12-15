aff2f7c925a177e3517c28dabeb6c2c7cb3ce800
christineLB <ch.lebruchec@gmail.com>
2016-12-15 10:18:36 +0100

    insert user

--- app/Controller/DefaultController.php
+++ app/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace Controller;
 
 use \W\Controller\Controller;
+use \W\Model\UsersModel;
+use \W\Security\AuthentificationModel;
 
 class DefaultController extends Controller
 {
@@ -13,6 +15,51 @@ class DefaultController extends Controller
 	public function home()
 	{
 		$this->show('default/home');
+	}
+
+		// Créer et insérer un nouvel utilisateur 
+	public function insertUser()
+	{
+		//$this->allowTo('admin');
+		$UsersModel = new UsersModel();
+		$authModel = new AuthentificationModel();
+
+		if(isset($_POST['insert-user'])) {
+			$errors = [];
+
+			if(empty($_POST['mail'])) {
+				$errors['mail']['empty'] = true;
+			}
+			if(empty($_POST['pass1'])) {
+				$errors['pass1']['empty'] = true;
+			}	
+			if(empty($_POST['firstname'])) {
+				$errors['firstname']['empty'] = true;
+			}
+			if(empty($_POST['lastname'])) {
+				$errors['lastname']['empty'] = true;
+			}
+			
+			if(count($errors) === 0) {
+				// Ajouter si OK
+				$UsersModel->insert([
+					'mail' 		=> $_POST['mail'],
+					'password' 	=> $authModel->hashPassword($_POST['pass1']),
+					'firstname' => $_POST['firstname'],
+					'lastname' 	=> $_POST['lastname'],
+					'nb_tries' 	=> 0,
+
+				]);
+				
+				// redirection vers connexion
+			}
+			else {
+				$this->show('users/add', ['errors' => $errors]);
+			}
+
+				
+		}
+			$this->show('users/add');
 	}
 
 }
--- app/routes.php
+++ app/routes.php
@@ -4,6 +4,8 @@
 		['GET', 		'/', 										'Default#home', 			'default_home'],
 		['GET|POST',	'/login',									'Default#login',			'default_login'],
 		['GET',			'/logout',									'Default#logout',			'default_logout'],
-		['GET|POST',	'/user/password-recovery',					'Default#passwordRecovery',	'default_password_recovery'],
-		['GET|POST',	'/user/reset-password/[:token]',			'Default#resetPassword',	'default_reset_password'],
+		['GET|POST',	'/users/password-recovery',					'Default#passwordRecovery',	'default_password_recovery'],
+		['GET|POST',	'/users/reset-password/[:token]',			'Default#resetPassword',	'default_reset_password'],
+		['GET|POST',	'/users/add',								'Default#insertUser',		'default_add'],
+
 	);
