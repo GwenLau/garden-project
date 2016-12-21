51533e9fc0f4623de48331b0797c4726b2c6cdb9
David Jankovic <davidjankovic0703@gmail.com>
2016-12-21 16:33:36 +0100

    creation pseudo

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -64,7 +64,7 @@
 		      <ul class="nav navbar-nav navbar-right">
 		        <!-- <li><a href="#">A propos</a></li> -->
 		        <li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profil de :<?= $user['firstname'] ?> <span class="caret"></span></a>
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profil de :<?= $user['pseudo'] ?> <span class="caret"></span></a>
 		          <ul class="dropdown-menu">
 		            <li><a href="<?= $this->url('default_dashboard') ?>">Dashboard</a></li>
 		            <li><a href="<?= $this->url('default_logout') ?>">Déconnection</a></li>
--- app/Views/users/sidebar_dashboard.php
+++ app/Views/users/sidebar_dashboard.php
@@ -8,7 +8,7 @@
 		<!-- SIDEBAR USER TITLE -->
 		<div class="profile-usertitle">
 			<div class="profile-usertitle-name">
-				<?= $user['firstname'] ?>
+				<?= $user['pseudo'] ?>
 			</div>
 		</div>
 		<!-- END SIDEBAR USER TITLE -->
