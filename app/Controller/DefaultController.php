e63f7470e881029a705158e937391598871bd31f
David Jankovic <davidjankovic0703@gmail.com>
2017-01-03 16:10:33 +0100

    envoi sms

--- app/Controller/DefaultController.php
+++ app/Controller/DefaultController.php
@@ -201,6 +201,17 @@ EOT;
 						'id_receive' => $_POST['destinataire'],
 						'Message' => $_POST['message'],
 					]);
+					$sid = "ACf25767309ce67abfed16cbacaab0a4f3"; // Your Account SID from www.twilio.com/console
+					$token = "73c2331465d98fb87b03c4aea6afb761"; // Your Auth Token from www.twilio.com/console
+
+					$client = new Client($sid, $token);
+					$message = $client->messages->create(
+					  '+33677062090', // Text this number
+					  array(
+					    'from' => '+33644641630', // From a valid Twilio number
+					    'body' => 'Bonjour vous avez une demande de jardin YoupiGarden!'
+					  )
+					);
 				} else {
 					$error = "Veuillez compléter tous les champs";
 				}
@@ -292,16 +303,16 @@ EOT;
 						'Message' => $_POST['message'],
 					]);
 					$sid = "ACf25767309ce67abfed16cbacaab0a4f3"; // Your Account SID from www.twilio.com/console
-			$token = "73c2331465d98fb87b03c4aea6afb761"; // Your Auth Token from www.twilio.com/console
+					$token = "73c2331465d98fb87b03c4aea6afb761"; // Your Auth Token from www.twilio.com/console
 
-			$client = new Client($sid, $token);
-			$message = $client->messages->create(
-			  '+33677062090', // Text this number
-			  array(
-			    'from' => '+33644641630', // From a valid Twilio number
-			    'body' => 'Bonjour vous avez une demande de jardin YoupiGarden!'
-			  )
-			);
+					$client = new Client($sid, $token);
+					$message = $client->messages->create(
+					  '+33677062090', // Text this number
+					  array(
+					    'from' => '+33644641630', // From a valid Twilio number
+					    'body' => 'Bonjour vous avez une demande de jardin YoupiGarden!'
+					  )
+					);
 				} else {
 					$error = "Veuillez compléter tous les champs";
 				}
